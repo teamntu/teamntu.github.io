9d3e4b4fa5aa0ade22c28184071cf5d3ebde0519
insectwang <frankwang50302@gmail.com>
2015-09-19 01:06:45 +0800

    change background

--- js/button.js
+++ js/button.js
@@ -8,15 +8,17 @@ $(function(){
     })
     .on('mouseover', function(){
         var $btn = $(this).stop(true).animate({
-            backgroundColor: 'black',
-            color: '#000'
+            backgroundColor: '#191970',
+			opacity:'0.7',
+            color: '#000',
         }, duration);
         $btn.find('img:first-child').stop(true).animate({opacity: 0}, duration);
         $btn.find('img:nth-child(2)').stop(true).animate({opacity: 1}, duration);
     })
     .on('mouseout', function(){
         var $btn = $(this).stop(true).animate({
-            backgroundColor: 'black',
+            backgroundColor: '#191970',
+			opacity:'0.7',
             color: '#01b169',
         }, duration);
         $btn.find('img:first-child').stop(true).animate({opacity: 1}, duration);
