6a7007d5b610ec03a74e6d738f8b1607e1f62cc7
insectwang <frankwang50302@gmail.com>
2015-09-20 00:24:15 +0800

    mobilenavbar

--- js/stickyheader.js
+++ js/stickyheader.js
@@ -14,3 +14,22 @@ $(function(){
         cubuk_seviye = $(document).scrollTop();	
      });
 });
+
+
+
+$(function(){	
+    var cubuk_seviye = $(document).scrollTop();
+    var header_yuksekligi = $('.mobilenav').outerHeight();
+
+    $(window).scroll(function() {
+        var kaydirma_cubugu = $(document).scrollTop();
+
+        if (kaydirma_cubugu < header_yuksekligi){$('.mobilenav').addClass('header2');} 
+        else {$('.mobilenav').removeClass('header');}
+
+        if (kaydirma_cubugu < cubuk_seviye){$('.mobilenav').removeClass('gizle');} 
+        else {$('.mobilenav').addClass('gizle');}				
+
+        cubuk_seviye = $(document).scrollTop();	
+     });
+});
