02639643360d51a5d60aaa3417afbdcda407db14
insectwang <frankwang50302@gmail.com>
2015-10-09 21:29:55 +0800

    sidemenu

--- js/scrolling.js
+++ js/scrolling.js
@@ -37,3 +37,47 @@ $("#page5").click(function() {
     }, 3000,'easeOutQuart');
 });
 });
+
+
+
+
+
+$(function(){
+$("#pa1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p1").offset().top
+    }, 3000,'easeOutQuart');
+});
+});
+
+$(function(){
+$("#pa2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p2").offset().top
+    }, 3000,'easeOutQuart');
+});
+});
+
+$(function(){
+$("#pa3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p3").offset().top
+    }, 3000,'easeOutQuart');
+});
+});
+
+$(function(){
+$("#pa4").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p4").offset().top
+    }, 3000,'easeOutQuart');
+});
+});
+
+$(function(){
+$("#pa5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#p5").offset().top
+    }, 3000,'easeOutQuart');
+});
+});
---
+++ js/sidemenu.js
@@ -0,0 +1,24 @@
+$(function(){
+
+	var duration = 300;
+
+	var $aside = $('.page-main > aside');
+	var $asideButton = $aside.find('button')
+		.on('click', function(){
+			$aside.toggleClass('open');
+			if($aside.hasClass('open')){
+				$aside.stop(true).animate({
+					left:'0px'
+				}, duration, 'easeOutBack');
+
+				$asidButton.find('img')
+					.attr('src', 'img/btn_close.png');
+			}else{
+				$aside.stop(true).animate({
+					left: '-210px'
+				}, duration, 'easeInBack');
+				$asidButton.find('img')
+					.attr('src', 'img/btn_open.png');
+			};
+		});
+});
