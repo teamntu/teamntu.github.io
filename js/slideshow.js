d0b874767369f5d20236847f2dfb38012da1ecc1
XDD <XDD>
2015-10-25 02:39:47 +0800

    wheeeeeeee

--- js/slideshow.js
+++ js/slideshow.js
@@ -23,3 +23,31 @@ $(function () {
     });
 
 });
+
+
+
+$(function () {
+
+  
+    $('.slideshow2').each(function () {
+
+        var $slides = $(this).find('img'), 
+            slideCount = $slides.length, 
+            currentIndex = 0;              
+        
+        $slides.eq(currentIndex).fadeIn();
+        setInterval(showNextSlide, 3000);
+
+        function showNextSlide () {
+
+						
+            var nextIndex = (currentIndex + 1) % slideCount;
+
+            $slides.eq(currentIndex).fadeOut();
+            $slides.eq(nextIndex).fadeIn();
+            currentIndex = nextIndex;
+        }
+
+    });
+
+});
